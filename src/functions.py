7787ff1d09c15065d02297bfa0393f462ae89183
dyu <dyu@eztexting.com>
2024-06-11 03:44:32 -0700

    Updated 2024-06-11 Premarket

--- src/functions.py
+++ src/functions.py
@@ -42,7 +42,6 @@ def current_time():
     return f'{today} {now}'
     
 
-
 def download_tables(stock_list = read_symbols_csv()):
     
     start_time = current_time()
@@ -61,6 +60,34 @@ def download_tables(stock_list = read_symbols_csv()):
                                     auto_adjust=False, # new as of 1/23/24
                                    )
         stock_1d_df.to_pickle(f'./data/{item[0]}_1d_df.pkl')
+        
+        #get max 1 hour data
+        stock_1h_df = stock.history(interval = '1h',  # time spacing interval
+                                    period='60d',  # historical period, can use start and end
+                                    auto_adjust=False, # new as of 1/23/24
+                                   )
+        stock_1h_df.to_pickle(f'./data/{item[0]}_1h_df.pkl')
+        
+        ctn += 1
+    
+    end_time = current_time()
+        
+    return print(f'Start time: {start_time}\nDownloaded {ctn} max daily and hourly stock data\nEnd Time: {end_time}')
+
+
+def load_transform_tables(stock_list = read_symbols_csv()):
+    
+    start_time = current_time()
+    
+    ctn = 0
+    
+    for item in stock_list:
+        
+        #get max 1 day data
+        stock_1d_df = rp(f'./data/{item[0]}_1d_df.pkl')
+        
+        
+        
         
         #get max 1 hour data
         stock_1h_df = stock.history(interval = '1h',  # time spacing interval
